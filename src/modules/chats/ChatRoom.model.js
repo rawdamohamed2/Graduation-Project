f8d53cd9cf49f4e220c62f224de839f6e4f54f68
Rawda mohamed <Rawdamohamed618@gmail.com>
2026-03-02 21:44:11 +0200

    Configure project as ES module and update .gitignore

--- src/modules/chats/ChatRoom.model.js
+++ src/modules/chats/ChatRoom.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const chatRoomSchema = new mongoose.Schema({
     participants: [
--- src/modules/subscription/SubscriptionPlan.model.js
+++ src/modules/subscription/SubscriptionPlan.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const subscriptionPlanSchema = new mongoose.Schema({
     name: {
